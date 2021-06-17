ebb94ce2b4f980bd45634d772f22d9c512d0953f
Guillaume Orsal (acotango) <guillaume@orsal.net>
2021-06-17 18:47:05 +0200

    Seed des users

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,17 +2,17 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory()->create([
+            'email' => 'dev@gsis.com',
+        ]);
+
+        User::factory(5)->create();
     }
 }
