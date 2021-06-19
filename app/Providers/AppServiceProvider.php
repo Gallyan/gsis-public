c81f29dd7cc26347155f8890c4fd6227f512b1c1
Guillaume Orsal (acotango) <guillaume@orsal.net>
2021-06-19 17:51:34 +0200

    Spatie permission prerequisite and phone validator

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Validator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,17 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // For length to get indexes on string field, spatie permission prerequisite
+        Schema::defaultStringLength(125);
+
+        // Add a validation rule for phone numbers
+        Validator::extend('phone', function($attribute, $value, $parameters, $validator) {
+            return preg_match('%^(?:(?:\(?(?:00|\+)([1-4]\d\d|[1-9]\d?)\)?)?[\-\.\ \\\/]?)?((?:\(?\d{1,}\)?[\-\.\ \\\/]?){0,})(?:[\-\.\ \\\/]?(?:#|ext\.?|extension|x)[\-\.\ \\\/]?(\d+))?$%i', $value) && ( strlen($value) >= 10 || strlen(trim($value)) == 0 );
+        });
+
+        Validator::replacer('phone', function($message, $attribute, $rule, $parameters) {
+            return __('Invalid phone number');
+        });
+
     }
 }
