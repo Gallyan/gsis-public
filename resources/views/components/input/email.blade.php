2ec3f55fdef1d97d77111d1e250d6808b6140faf
Guillaume Orsal (acotango) <guillaume@orsal.net>
2021-06-19 17:09:46 +0200

    Input text with icon

---
+++ resources/views/components/input/email.blade.php
@@ -0,0 +1,7 @@
+<div class="flex rounded-md shadow-sm">
+    <span class="inline-flex items-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500 sm:text-sm">
+        <x-icon.email class="h-5 w-5 text-gray-400" />
+    </span>
+
+    <input {{ $attributes->merge(['class' => 'flex-1 form-input border-cool-gray-300 block w-full transition duration-150 ease-in-out sm:text-sm sm:leading-5 rounded-none rounded-r-md']) }}/>
+</div>
