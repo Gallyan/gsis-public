1841141cd93f32bbad8e8145608026a1f8f4abb1
Guillaume Orsal (acotango) <guillaume@orsal.net>
2021-06-19 15:46:57 +0200

    Preparation a la localisation

--- resources/views/components/input/group.blade.php
+++ resources/views/components/input/group.blade.php
@@ -10,35 +10,35 @@
 
 @if($inline)
     <div>
-        <label for="{{ $for }}" class="block text-sm font-medium leading-5 text-gray-700">{{ $label }}</label>
+        <label for="{{ $for }}" class="block text-sm font-medium leading-5 text-gray-700">{{ __($label) }}</label>
 
         <div class="mt-1 relative rounded-md shadow-sm">
             {{ $slot }}
 
             @if ($error)
-                <div class="mt-1 text-red-500 text-sm">{{ $error }}</div>
+                <div class="mt-1 text-red-500 text-sm">{{ __($error) }}</div>
             @endif
 
             @if ($helpText)
-                <p class="mt-2 text-sm text-gray-500">{{ $helpText }}</p>
+                <p class="mt-2 text-sm text-gray-500">{{ __($helpText) }}</p>
             @endif
         </div>
     </div>
 @else
     <div class="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start {{ $borderless ? '' : ' sm:border-t ' }} sm:border-gray-200 {{ $paddingless ? '' : ' sm:py-5 ' }}">
         <label for="{{ $for }}" class="block text-sm font-medium leading-5 text-gray-700 sm:mt-px sm:pt-2">
-            {{ $label }}
+            {{ __($label) }}
         </label>
 
         <div class="mt-1 sm:mt-0 sm:col-span-2">
             {{ $slot }}
 
             @if ($error)
-                <div class="mt-1 text-red-500 text-sm">{{ $error }}</div>
+                <div class="mt-1 text-red-500 text-sm">{{ __($error) }}</div>
             @endif
 
             @if ($helpText)
-                <p class="mt-2 text-sm text-gray-500">{{ $helpText }}</p>
+                <p class="mt-2 text-sm text-gray-500">{{ __($helpText) }}</p>
             @endif
         </div>
     </div>
