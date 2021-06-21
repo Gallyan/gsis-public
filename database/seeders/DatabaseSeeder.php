767592df8a284df767ed5951f0fe3b071edc678c
Guillaume Orsal (acotango) <guillaume@orsal.net>
2021-06-21 10:54:43 +0200

    Amelioration du seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,25 +4,30 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
 
 class DatabaseSeeder extends Seeder
 {
     public function run()
     {
-        User::factory()->create([
-            'email' => 'admin@gsis.com',
-        ]);
+        // Creation des roles
+		$admin = Role::create(['name' => 'admin']);
+		$manager = Role::create(['name' => 'manager']);
+		$user = Role::create(['name' => 'user']);
 
-        User::factory()->create([
-            'email' => 'manager@gsis.com',
-        ]);
+        // Creation des permissions
+        Permission::create(['name' => 'manage-users']);
 
-        User::factory()->create([
-            'email' => 'user@gsis.com',
-        ]);
+        // Assign permissions to roles
+        $manager->givePermissionTo('manage-users');
+        $admin->givePermissionTo('manage-users');
 
+        // Création des utilisateurs
+        User::factory()->create([ 'email' => 'admin@gsis.com' ])->assignRole('admin');
+        User::factory()->create([ 'email' => 'manager@gsis.com' ])->assignRole('manager');
+        User::factory()->create([ 'email' => 'user@gsis.com' ])->assignRole('user');
         User::factory(5)->create();
-
-        $this->call(RolesTableSeeder::class);
     }
 }
