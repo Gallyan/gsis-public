1bf64057cd9a04c727e431478924235ab6c72c5b
Guillaume Orsal (acotango) <guillaume@orsal.net>
2021-06-21 14:41:53 +0200

    Initial user list

--- app/Http/Livewire/Users.php
+++ app/Http/Livewire/Users.php
@@ -3,11 +3,15 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use App\Models\User;
+use Illuminate\Support\Carbon;
 
 class Users extends Component
 {
     public function render()
     {
-        return view('livewire.users');
+        return view('livewire.users', [
+            'users' => User::paginate(10),
+        ]);
     }
 }
--- resources/views/livewire/users.blade.php
+++ resources/views/livewire/users.blade.php
@@ -1,14 +1,71 @@
 <div>
     <h1 class="text-2xl font-semibold text-gray-900">{{ __('Users') }}</h1>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    Users List
-                </div>
+    <div class="py-4 space-y-4">
+        <!-- Users Table -->
+        <div class="flex-col space-y-4">
+            <x-table>
+                <x-slot name="head">
+                    <x-table.heading sortable multi-column wire:click="sortBy('name')" :direction="$sorts['name'] ?? null" class="w-full">{{ __('Name') }}</x-table.heading>
+                    <x-table.heading sortable multi-column wire:click="sortBy('email')" :direction="$sorts['email'] ?? null">{{ __('Email') }}</x-table.heading>
+                    <x-table.heading sortable multi-column wire:click="sortBy('roles')" :direction="$sorts['roles'] ?? null">{{ __('Roles') }}</x-table.heading>
+                    <x-table.heading sortable multi-column wire:click="sortBy('date')" :direction="$sorts['date'] ?? null">{{ __('Created') }}</x-table.heading>
+                    <x-table.heading />
+                </x-slot>
+
+                <x-slot name="body">
+                    @forelse ($users as $user)
+                    <x-table.row wire:loading.class.delay="opacity-50" wire:key="row-{{ $user->id }}">
+                        <x-table.cell>
+                            <span href="#" class="inline-flex space-x-2 truncate text-sm leading-5">
+                                <p class="text-cool-gray-600 truncate">
+                                    {{ $user->full_name }}
+                                </p>
+                            </span>
+                        </x-table.cell>
+
+                        <x-table.cell>
+                            <span href="#" class="inline-flex space-x-2 truncate text-sm leading-5">
+                                <p class="text-cool-gray-600 truncate">
+                                    {{ $user->email }}
+                                    @if ( $user->verified === true )
+                                    <x-icon.check class="text-green-400" />
+                                    @elseif ( $user->verified === false )
+                                    <x-icon.x class="text-red-400" />
+                                    @endif
+                                </p>
+                            </span>
+                        </x-table.cell>
+
+                        <x-table.cell>
+                            {{ ucwords( $user->roles_names ) }}
+                        </x-table.cell>
+
+                        <x-table.cell>
+                            {{ $user->date_for_humans }}
+                        </x-table.cell>
+
+                        <x-table.cell>
+                            <x-button.link wire:click="edit({{ $user->id }})">{{ __('Edit') }}</x-button.link>
+                        </x-table.cell>
+                    </x-table.row>
+                    @empty
+                    <x-table.row>
+                        <x-table.cell colspan="6">
+                            <div class="flex justify-center items-center space-x-2">
+                                <x-icon.inbox class="h-8 w-8 text-cool-gray-400" />
+                                <span class="font-medium py-8 text-cool-gray-400 text-xl">No users found...</span>
+                            </div>
+                        </x-table.cell>
+                    </x-table.row>
+                    @endforelse
+                </x-slot>
+            </x-table>
+
+            <div>
+                {{ $users->links() }}
             </div>
+
         </div>
     </div>
-    
 </div>
