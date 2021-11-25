8fa20ee8a6797a77784f7d770a88996e9488f58c
Guillaume Orsal (acotango) <guillaume@orsal.net>
2021-11-25 19:15:42 +0100

    Ajout du bandeau sur l'acceuil

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>{{ config('app.name', 'Gsis 2') }}</title>
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
@@ -16,8 +16,12 @@
         <!-- Scripts -->
         <script src="{{ asset('js/app.js') }}" defer></script>
     </head>
-    <body>
-        <div class="font-sans text-gray-900 antialiased">
+    <body class="font-sans text-gray-900 antialiased">
+        @env(['preprod','local'])
+            <div class="fixed bg-red-500 text-white text-sm font-bold px-10 py-1 transform -rotate-45 -left-10 top-4 text-center w-32">{{ ucfirst(env('APP_ENV')) }}</div>
+        @endenv
+
+        <div>
             {{ $slot }}
         </div>
     </body>
