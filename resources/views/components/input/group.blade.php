80da38251a6116bbc6231f164b3aaded4c94decf
Guillaume Orsal (acotango) <guillaume@orsal.net>
2021-12-16 14:05:20 +0100

    Default value for props

--- resources/views/components/input/group.blade.php
+++ resources/views/components/input/group.blade.php
@@ -1,6 +1,6 @@
 @props([
-    'label',
-    'for',
+    'label' => '',
+    'for' => '',
     'error' => false,
     'helpText' => false,
     'inline' => false,
@@ -11,7 +11,9 @@
 
 @if($inline)
     <div>
+        @if($label)
         <label for="{{ $for }}" class="block text-sm font-medium leading-5 text-gray-700 {{ $required ? 'required' : '' }}">{{ __($label) }}</label>
+        @endif
 
         <div class="mt-1 relative rounded-md shadow-sm">
             {{ $slot }}
@@ -27,9 +29,11 @@
     </div>
 @else
     <div class="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start {{ $borderless ? '' : ' sm:border-t ' }} sm:border-gray-200 {{ $paddingless ? '' : ' sm:py-5 ' }}">
+        @if($label)
         <label for="{{ $for }}" class="block text-sm font-medium leading-5 text-gray-700 sm:mt-px pt-2 {{ $required ? 'required' : '' }}">
             {{ __($label) }}
         </label>
+        @endif
 
         <div class="mt-1 sm:mt-0 sm:col-span-2">
             {{ $slot }}
