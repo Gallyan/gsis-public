0978a7fc2529b37d00347fd5d66222c3aed7a4c3
Guillaume Orsal (acotango) <guillaume@orsal.net>
2022-01-01 18:08:50 +0100

    Add heading with small height

--- resources/views/components/table/heading.blade.php
+++ resources/views/components/table/heading.blade.php
@@ -2,10 +2,11 @@
     'sortable' => null,
     'direction' => null,
     'multiColumn' => null,
+    'small' => null,
 ])
 
 <th
-    {{ $attributes->merge(['class' => 'px-6 py-3 bg-cool-gray-50'])->only('class') }}
+    {{ $attributes->only('class')->merge(['class' => 'px-6 bg-cool-gray-50 '.($small ? 'py-0' : 'py-3')]) }}
 >
     @unless ($sortable)
         <span class="text-left text-xs leading-4 font-medium text-cool-gray-500 uppercase tracking-wider">{{ $slot }}</span>
