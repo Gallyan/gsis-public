38e230149e1425511a0d0b95295b0b1e86651a0b
Guillaume Orsal (acotango) <guillaume@orsal.net>
2022-01-01 17:16:20 +0100

    Add class parameter to a group in a list

--- resources/views/components/input/group.blade.php
+++ resources/views/components/input/group.blade.php
@@ -7,10 +7,11 @@
     'paddingless' => false,
     'borderless' => false,
     'required' => false,
+    'class' => '',
 ])
 
 @if($inline)
-    <div>
+    <div class="{{ $class }}">
         @if($label)
         <label for="{{ $for }}" class="block text-sm font-medium leading-5 text-gray-700 {{ $required ? 'required' : '' }}">{{ __($label) }}</label>
         @endif
@@ -28,14 +29,14 @@
         </div>
     </div>
 @else
-    <div class="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start {{ $borderless ? '' : ' sm:border-t ' }} sm:border-gray-200 {{ $paddingless ? '' : ' sm:py-5 ' }}">
+    <div class="sm:grid sm:grid-cols-5 sm:gap-4 sm:items-start {{ $borderless ? '' : ' sm:border-t ' }} sm:border-gray-200 {{ $paddingless ? '' : ' sm:py-5 ' }} {{ $class }}">
         @if($label)
         <label for="{{ $for }}" class="block text-sm font-medium leading-5 text-gray-700 sm:mt-px pt-2 {{ $required ? 'required' : '' }}">
             {{ __($label) }}
         </label>
         @endif
 
-        <div class="mt-1 sm:mt-0 sm:col-span-2">
+        <div class="mt-1 sm:mt-0 sm:col-span-4">
             {{ $slot }}
 
             @if ($error)
