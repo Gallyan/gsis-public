a79d8cefd9e11c4c4990f946949790b2628f4272
Guillaume Orsal (acotango) <guillaume@orsal.net>
2022-01-02 22:00:28 +0100

    typo

--- resources/views/livewire/institutions.blade.php
+++ resources/views/livewire/institutions.blade.php
@@ -97,7 +97,7 @@
     <!-- Save Institution Modal -->
     <form wire:submit.prevent="save">
         <x-modal.dialog wire:model.defer="showEditModal">
-            <x-slot name="title">{{ __('Edit Insititution') }}</x-slot>
+            <x-slot name="title">{{ __('Edit Institution') }}</x-slot>
 
             <x-slot name="content">
                 <x-input.group for="name" label="Name" :error="$errors->first('editing.name')" required>
