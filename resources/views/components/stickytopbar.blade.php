d65653c4cac891f7c93765beb948167b6892dce8
Guillaume Orsal (acotango) <guillaume@orsal.net>
2022-01-06 17:33:46 +0100

    Use component in sticky bar

--- resources/views/components/stickytopbar.blade.php
+++ resources/views/components/stickytopbar.blade.php
@@ -7,32 +7,16 @@
 
     <div class="flex-grow">
         <div class="flex flex-col">
-            <div class="flex-1 space-x-3 flex justify-end items-center">
-                <span x-data="{ open: false }" x-init="
-                        @this.on('notify-saved', () => {
-                            if (open === false) setTimeout(() => { open = false }, 2500);
-                            open = true;
-                        })
-                    " x-show.transition.out.duration.1000ms="open" style="display: none;" class="inline-flex text-green-600">{{ __('Saved!') }}</span>
+            <div class="flex-1 space-x-3 flex justify-end items-center mt-1">
 
-                <span x-data="{ open: false }" x-init="
-                        @this.on('notify-error', () => {
-                            if (open === false) setTimeout(() => { open = false }, 2500);
-                            open = true;
-                        })
-                    " x-show.transition.out.duration.1000ms="open" style="display: none;" class="inline-flex text-red-600">{{ __('Error') }}</span>
+                <x-notify-message event='notify-saved' color='text-green-600'>{{ __('Saved!') }}</x-notify-message>
 
-                <span class="inline-flex rounded-md shadow-sm">
-                    <button type="reset" class="mt-1 py-2 px-4 border border-gray-300 rounded-md text-sm leading-5 font-medium text-gray-700 hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-50 active:text-gray-800 transition duration-150 ease-in-out">
-                        {{ __('Reset') }}
-                    </button>
-                </span>
+                <x-notify-message event='notify-error' color='text-red-600'>{{ __('Error!') }}</x-notify-message>
 
-                <span class="inline-flex rounded-md shadow-sm">
-                    <button type="submit" wire:offline.attr="disabled" class="mt-1 py-2 px-4 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 active:bg-indigo-700 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo disabled:bg-gray-300 transition duration-150 ease-in-out">
-                        {{ __('Save') }}
-                    </button>
-                </span>
+                <x-button.secondary type="reset">{{ __('Reset') }}</x-button.primary>
+
+                <x-button.primary type="submit" wire:offline.attr="disabled">{{ __('Save') }}</x-button.primary>
+
             </div>
             <div wire:offline class="mt-1 text-red-500 text-xs sm:text-sm text-right">
                 You are now offline. You can't save modification.
