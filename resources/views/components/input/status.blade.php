ffa031b04f2d0685830735f9a28d3af31ab50a25
Guillaume Orsal (acotango) <guillaume@orsal.net>
2022-01-20 13:51:44 +0100

    Update

--- resources/views/components/input/status.blade.php
+++ resources/views/components/input/status.blade.php
@@ -17,7 +17,7 @@
 
       <label
         class="group flex items-center justify-center w-full cursor-pointer transition duration-300 ease-in-out
-          @if ( !empty( $disabled_keys ) && in_array( $key, $disabled_keys ) ) bg-gray-300 opacity-75 cursor-not-allowed @endif
+          @if ( !empty( $disabled_keys ) && is_array( $disabled_keys ) && in_array( $key, $disabled_keys ) ) bg-gray-300 opacity-75 cursor-not-allowed @endif
           @if ( $this->order->status == $key ) text-indigo-600  hover:text-indigo-800 border-indigo-600
           @else text-gray-500 hover:text-gray-700  border-gray-300 @endif bg-gray-50 hover:bg-gray-100"
         for="{{ $key }}" >
@@ -25,7 +25,7 @@
         <span class="px-6 py-2 flex items-center justify-center">
 
           <span class="flex-shrink-0 w-5 h-5 flex items-center justify-center border-2 @if ( $this->order->status == $key ) border-indigo-600 @else border-gray-200 @endif rounded-full text-xs">
-            @if ( $this->order->status == $key )
+            @if ( $this->order->status === $key )
               <x-icon.check />
             @else
               {{ $loop->iteration }}
