2f6fde4a5ed2ab764b13ec5ad005928199f90b73
Guillaume Orsal (acotango) <guillaume@orsal.net>
2022-01-20 11:12:23 +0100

    Input checkbox component

--- resources/views/components/input/checkbox.blade.php
+++ resources/views/components/input/checkbox.blade.php
@@ -1,6 +1,15 @@
-<div class="flex rounded-md shadow-sm">
-    <input {{ $attributes }}
-        type="checkbox"
-        class="form-checkbox border-cool-gray-300 block transition duration-150 ease-in-out sm:text-sm sm:leading-5"
-    />
+@props([
+    'for',
+])
+
+<div class="relative flex items-center h-10 px-3">
+    <div class="flex items-center h-5">
+        <input {{ $attributes }}
+            type="checkbox"
+            class="form-checkbox focus:ring-cyan-600 h-4 w-4 text-cyan-600 border-gray-400 rounded transition duration-150 ease-in-out sm:text-sm sm:leading-5"
+        />
+    </div>
+    <div class="ml-3 text-sm">
+      <label for="{{ $for }}" class="block text-sm font-medium leading-5 text-gray-700">{{ $slot }}</label>
+    </div>
 </div>
