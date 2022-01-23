322ca44bc6c88bc6d6bf972901518d79c4f16e06
Guillaume Orsal (acotango) <guillaume@orsal.net>
2022-01-23 13:44:46 +0100

    Filepond translation

--- resources/views/components/input/filepond.blade.php
+++ resources/views/components/input/filepond.blade.php
@@ -6,6 +6,27 @@
         pond.setOptions({
             allowMultiple: {{ isset($attributes['multiple']) ? 'true' : 'false' }},
             labelIdle: '{{ __('filepondIdle') }}',
+            labelFileProcessingComplete: '{{ __('filepondFileProcessingComplete') }}',
+            labelInvalidField: '{{ __('filepondInvalidField') }}',
+            labelFileWaitingForSize: '{{ __('filepondFileWaitingForSize') }}',
+            labelFileSizeNotAvailable: '{{ __('filepondFileSizeNotAvailable') }}',
+            labelFileLoading: '{{ __('filepondFileLoading') }}',
+            labelFileLoadError: '{{ __('filepondFileLoadError') }}',
+            labelFileProcessing: '{{ __('filepondFileProcessing') }}',
+            labelFileProcessingAborted: '{{ __('filepondFileProcessingAborted') }}',
+            labelFileProcessingError: '{{ __('filepondFileProcessingError') }}',
+            labelFileProcessingRevertError: '{{ __('filepondFileProcessingRevertError') }}',
+            labelFileRemoveError: '{{ __('filepondFileRemoveError') }}',
+            labelTapToCancel: '{{ __('filepondTapToCancel') }}',
+            labelTapToRetry: '{{ __('filepondTapToRetry') }}',
+            labelTapToUndo: '{{ __('filepondTapToUndo') }}',
+            labelButtonRemoveItem: '{{ __('filepondButtonRemoveItem') }}',
+            labelButtonAbortItemLoad: '{{ __('filepondButtonAbortItemLoad') }}',
+            labelButtonRetryItemLoad: '{{ __('filepondButtonRetryItemLoad') }}',
+            labelButtonAbortItemProcessing: '{{ __('filepondButtonAbortItemProcessing') }}',
+            labelButtonUndoItemProcessing: '{{ __('filepondButtonUndoItemProcessing') }}',
+            labelButtonRetryItemProcessing: '{{ __('filepondButtonRetryItemProcessing') }}',
+            labelButtonProcessItem: '{{ __('filepondButtonProcessItem') }}',
             server: {
                 process: (fieldName, file, metadata, load, error, progress, abort, transfer, options) => {
                     @this.upload('{{ $attributes['wire:model'] }}', file, load, error, progress)
