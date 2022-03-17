d34ac6ffc82d93efc718ebbda2021cd87215ce20
Guillaume Orsal (acotango) <guillaume@orsal.net>
2022-03-17 14:23:02 +0100

    Default Orders order by created_at desc

--- app/Http/Livewire/Orders.php
+++ app/Http/Livewire/Orders.php
@@ -27,6 +27,11 @@ class Orders extends Component
 
     protected $queryString = ['sorts'];
 
+    public function mount() {
+        if( empty( $this->sorts) )
+            $this->sorts = [ "created_at" => "desc" ];
+    }
+
     public function toggleShowFilters() {
 
         $this->useCachedRows();
