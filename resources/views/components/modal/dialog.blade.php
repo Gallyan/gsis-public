e870e80918be834da5273fcccf88f997a0bc0181
Guillaume Orsal (acotango) <guillaume@orsal.net>
2022-03-29 18:43:40 +0200

    Add notify message to modal dialog box

--- resources/views/components/modal/dialog.blade.php
+++ resources/views/components/modal/dialog.blade.php
@@ -2,8 +2,10 @@
 
 <x-modal :id="$id" :maxWidth="$maxWidth" {{ $attributes }}>
     <div class="px-6 py-4">
-        <div class="text-lg">
+        <div class="text-lg flex">
             {{ $title }}
+            <x-notify-message event='dialog-saved' color='text-green-600 flex-1 justify-end'>{{ __('Saved!') }}</x-notify-message>
+            <x-notify-message event='dialog-error' color='text-red-600 flex-1 justify-end'>{{ __('Error!') }}</x-notify-message>
         </div>
 
         <div class="mt-4">
