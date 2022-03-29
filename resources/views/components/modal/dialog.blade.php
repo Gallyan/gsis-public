7e4b926fe069b583dc22587d8f40678fd70f8ce5
Guillaume Orsal (acotango) <guillaume@orsal.net>
2022-03-29 21:25:26 +0200

    code plus lisible

--- resources/views/components/modal/dialog.blade.php
+++ resources/views/components/modal/dialog.blade.php
@@ -4,8 +4,12 @@
     <div class="px-6 py-4">
         <div class="text-lg flex">
             {{ $title }}
-            <x-notify-message event='dialog-saved' color='text-green-600 flex-1 justify-end'>{{ __('Saved!') }}</x-notify-message>
-            <x-notify-message event='dialog-error' color='text-red-600 flex-1 justify-end'>{{ __('Error!') }}</x-notify-message>
+            <x-notify-message event='dialog-saved' color='text-green-600 flex-1 justify-end'>
+                {{ __('Saved!') }}
+            </x-notify-message>
+            <x-notify-message event='dialog-error' color='text-red-600 flex-1 justify-end'>
+                {{ __('Error!') }}
+            </x-notify-message>
         </div>
 
         <div class="mt-4">
