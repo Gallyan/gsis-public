9a750c6b280b8de560896d7386464caad0d81572
Guillaume Orsal (acotango) <guillaume@orsal.net>
2022-04-07 18:09:01 +0200

    Protected download

--- app/Http/Controllers/DocumentController.php
+++ app/Http/Controllers/DocumentController.php
@@ -2,11 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use Str;
+use App\Models\Document;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class DocumentController extends Controller
 {
     public function download($id) {
-        return 'Download document '.$id;
+        $doc = Document::findOrFail($id);
+
+        // Check rights : Owner or manager
+        if ( auth()->user() !== $doc->user_id && ! auth()->user()->can('manage-users') )
+            return abort(403);
+
+        // Create filename
+        $pathToFile = '/docs/' . $doc->user_id . '/' . $doc->filename;
+        if (empty($doc->name)) {
+            $filename = $doc->user_id . '-' . $doc->type . '-' . $doc->filename;
+        } else {
+            $extension = pathinfo( $pathToFile, PATHINFO_EXTENSION);
+            $filename = $doc->user_id . '-' . Str::slug( $doc->name ) . '.' . $extension;
+        }
+
+        // Check if file exist and download
+        if ( Storage::exists( $pathToFile ) ) {
+            return Storage::download($pathToFile, $filename);
+        }
+
+        return abort(404);
     }
 }
