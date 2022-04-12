651e49c7611ffb04fa1015a2d5f3528e3104aca0
Guillaume Orsal (acotango) <guillaume@orsal.net>
2022-04-12 10:08:53 +0200

    Ajout de commande laravel pour lister les fichiers inutiles

---
+++ app/Console/Commands/UnusedAvatars.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
+
+class UnusedAvatars extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'gsis:unused-avatars';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Liste les avatars qui ne sont plus affectés à un utilisateur';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $this->avatars = DB::table('users')->whereNotNull('avatar')->pluck('avatar')->toArray();
+
+        $files = Storage::files('avatars');
+
+        $to_delete = array_values( array_filter( $files, function( $file ) {
+            return $file[8] !== '.' && !in_array( substr($file,8), $this->avatars );
+        } ) );
+
+        foreach( $to_delete as $file ) $this->info( $file  );
+
+        $this->info( count($to_delete) . ' avatar(s) non affecté(s)' );
+    }
+}
---
+++ app/Console/Commands/UnusedDocuments.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
+
+class UnusedDocuments extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'gsis:unused-documents';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Liste les fichiers qui ne sont plus affectés à un object';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $documents = DB::table('documents')->whereNotNull('filename')->pluck('filename')->toArray();
+
+        $cpt = 0;
+
+        foreach( Storage::directories('docs') as $dir ) {
+
+            foreach( Storage::files($dir) as $file ) {
+
+                if ( ! in_array( preg_replace( '/\/?docs\/[0-9]+\//', '', $file ), $documents ) ) {
+
+                    $this->info( $file );
+
+                    $cpt++;
+
+                }
+
+            }
+        }
+
+        $this->info( $cpt . ' fichier(s) non associé(s)' );
+    }
+}
