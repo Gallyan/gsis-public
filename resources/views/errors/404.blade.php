46661c4971909650a41aa7f6cf3a17a4ef3ca17f
Guillaume Orsal (acotango) <guillaume@orsal.net>
2022-05-23 21:33:22 +0200

    Improve error pages

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -18,7 +18,7 @@
     </head>
     <body class="antialiased font-sans h-full">
 
-        <div class="bg-white min-h-full px-4 py-16 sm:px-6 sm:py-24 md:grid md:place-items-center lg:px-8">
+        <div class="bg-gray-100 min-h-full px-4 py-16 sm:px-6 sm:py-24 md:grid md:place-items-center lg:px-8">
             <div class="max-w-max mx-auto">
                 <div class="flex-shrink-0 flex justify-center">
                     <a href="/" class="inline-flex mb-6">
@@ -26,10 +26,10 @@
                     </a>
                 </div>
                 <main class="flex">
-                    <p class="text-4xl font-extrabold text-indigo-600 sm:text-5xl">404</p>
-                    <div class="sm:ml-6">
+                    <p class="text-4xl font-extrabold text-indigo-800 sm:text-5xl">404</p>
+                    <div class="ml-2 sm:ml-6">
                         <div class="sm:border-l sm:border-gray-200 sm:pl-6">
-                            <h1 class="text-4xl font-extrabold text-gray-900 tracking-tight sm:text-5xl">{{ __('Page not found.') }}</h1>
+                            <h1 class="text-4xl font-extrabold text-gray-700 tracking-tight sm:text-5xl">{{ __('Page not found.') }}</h1>
                             <p class="mt-1 text-base text-gray-500">{{ __('Sorry, we couldn’t find the page you’re looking for.') }}</p>
                         </div>
                     </div>
