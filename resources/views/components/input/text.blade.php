ce118b22d3ea03d1dfaa5e808d761935e1c8da40
Guillaume Orsal (acotango) <guillaume@orsal.net>
2022-06-02 23:04:13 +0200

    Uniformisation couleur

--- resources/views/components/input/text.blade.php
+++ resources/views/components/input/text.blade.php
@@ -4,7 +4,7 @@
 
 <div class="flex rounded-md shadow-sm">
     @if ($leadingAddOn)
-        <span class="inline-flex items-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500 sm:text-sm">
+        <span class="inline-flex items-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-400 sm:text-sm">
             {{ $leadingAddOn }}
         </span>
     @endif
