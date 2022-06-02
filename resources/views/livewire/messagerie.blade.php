a3d9b98905e2771b3ce6e2ae885f19da25db41c4
Guillaume Orsal (acotango) <guillaume@orsal.net>
2022-06-03 00:04:46 +0200

    css quand image non dispo

--- resources/views/livewire/messagerie.blade.php
+++ resources/views/livewire/messagerie.blade.php
@@ -6,7 +6,7 @@
                 <span class="absolute top-5 left-5 -ml-px h-full w-0.5 bg-gray-200" aria-hidden="true"></span>
                 <div class="relative flex items-start space-x-3">
                     <div class="relative">
-                        <img class="h-10 w-10 rounded-full bg-gray-100 flex items-center justify-center ring-1 ring-white" src="{{ auth()->user()->avatarUrl() }}" alt="{{ auth()->user()->name }}">
+                        <img class="h-10 w-10 rounded-full bg-gray-100 flex items-center justify-center text-xs text-gray-500 truncate ring-1 ring-white" src="{{ auth()->user()->avatarUrl() }}" alt="{{ auth()->user()->name }}">
                     </div>
                     <div class="min-w-0 flex-1">
                         <div>
@@ -30,7 +30,7 @@
                     @endif
                     <div class="relative flex items-start space-x-3">
                         <div class="relative">
-                            <img class="h-10 w-10 rounded-full truncate flex items-center bg-gray-100 justify-center ring-1 ring-white" src="{{ $post->user()->first()->avatarUrl() }}" alt="{{ $post->user()->first()->name }}">
+                            <img class="h-10 w-10 rounded-full bg-gray-100 flex items-center justify-center text-xs text-gray-500 truncate ring-1 ring-white" src="{{ $post->user()->first()->avatarUrl() }}" alt="{{ $post->user()->first()->name }}">
                         </div>
                         <div class="min-w-0 flex-1">
                             <div>
