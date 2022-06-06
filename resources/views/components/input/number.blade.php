d2b4cbcb98582a574c24efbd5b31915419c0b9c3
Guillaume Orsal (acotango) <guillaume@orsal.net>
2022-06-06 15:00:01 +0200

    number field

---
+++ resources/views/components/input/number.blade.php
@@ -0,0 +1,3 @@
+<div class="flex rounded-md shadow-sm">
+    <input {{ $attributes }} type="number" step="1" class="form-input border-gray-300 block w-full text-gray-700 transition duration-150 ease-in-out sm:text-sm sm:leading-5 rounded-md" placeholder="0" aria-describedby="price-currency">
+</div>
