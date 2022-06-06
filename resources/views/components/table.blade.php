e3b73885b3031cf58f9963d60b26263bbccd43c2
Guillaume Orsal (acotango) <guillaume@orsal.net>
2022-06-06 10:56:59 +0200

    Possibilité d'un tableau sans header

--- resources/views/components/table.blade.php
+++ resources/views/components/table.blade.php
@@ -1,10 +1,12 @@
 <div class="align-middle min-w-full overflow-x-auto shadow overflow-hidden sm:rounded-lg">
     <table class="min-w-full divide-y divide-cool-gray-200">
+        @isset ($head)
         <thead>
             <tr>
                 {{ $head }}
             </tr>
         </thead>
+        @endisset
 
         <tbody class="bg-white divide-y divide-cool-gray-200">
             {{ $body }}
