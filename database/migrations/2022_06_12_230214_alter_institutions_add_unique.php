2e3d62475fa33952016d27ef2f65160b9742d1ea
Guillaume Orsal (acotango) <guillaume@orsal.net>
2022-06-13 07:50:43 +0200

    Add unique index on institutions table

---
+++ database/migrations/2022_06_12_230214_alter_institutions_add_unique.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('institutions', function (Blueprint $table) {
+            $table->unique(['name', 'contract', 'allocation']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('institutions', function (Blueprint $table) {
+            $table->dropUnique('institutions_name_contract_allocation_unique');
+        });
+    }
+};
