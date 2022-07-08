1f1fa83f6e73c98fc0f836d9294ab8cb02458070
Guillaume Orsal (acotango) <guillaume@orsal.net>
2022-07-08 09:24:12 +0200

    Migrate WP field on purchases

---
+++ database/migrations/2022_07_08_085504_alter_purchases_convert_wp_to_integer.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\Models\Purchase;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        foreach( Purchase::whereNotNull('wp')->get() as $p ) {
+            $p->wp = substr( $p->wp, 2 );
+            $p->save();
+        }
+
+        Schema::table('purchases', function (Blueprint $table) {
+            $table->integer('wp')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('purchases', function (Blueprint $table) {
+            $table->string('wp')->nullable()->change();
+        });
+
+        foreach( Purchase::whereNotNull('wp')->get() as $p ) {
+            $p->wp = 'wp' . $p->wp;
+            $p->save();
+        }
+    }
+};
