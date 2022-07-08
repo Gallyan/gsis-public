7fb9376bb2b9b72b57c07d8dd55b4d6524112120
Guillaume Orsal (acotango) <guillaume@orsal.net>
2022-07-08 08:50:21 +0200

    Amélioration du champ number

--- resources/views/components/input/number.blade.php
+++ resources/views/components/input/number.blade.php
@@ -1,3 +1,3 @@
-<div class="flex rounded-md shadow-sm">
-    <input {{ $attributes }} type="number" step="1" class="form-input border-gray-300 block w-full text-gray-700 transition duration-150 ease-in-out sm:text-sm sm:leading-5 rounded-md" placeholder="0" aria-describedby="price-currency">
+<div class="flex shadow-sm">
+    <input {{ $attributes->merge(['class' => 'flex-1 form-input border-gray-300 text-gray-700 block w-full transition duration-150 ease-in-out sm:text-sm sm:leading-5 rounded-md']) }} type="number" step="1">
 </div>
