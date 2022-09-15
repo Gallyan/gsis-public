822d847252e0edb7dea67c3caa30cefa982b842c
Guillaume Orsal (acotango) <guillaume@orsal.net>
2022-09-15 13:14:24 +0200

    bugfix colspan

--- resources/views/livewire/institutions.blade.php
+++ resources/views/livewire/institutions.blade.php
@@ -84,7 +84,7 @@
                     </x-table.row>
                     @empty
                     <x-table.row>
-                        <x-table.cell colspan="4">
+                        <x-table.cell colspan="5">
                             <div class="flex justify-center items-center space-x-2">
                                 <x-icon.inbox class="h-8 w-8 text-gray-400" />
                                 <span class="font-medium py-8 text-gray-400 text-xl">{{ __('Nothing found...') }}</span>
