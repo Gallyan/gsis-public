e2cb68db35b663e2b3b3e4b67c46dc17985fd7c0
Guillaume Orsal (acotango) <guillaume@orsal.net>
2022-10-19 17:29:56 +0200

    Ajout du lieu de naissance

--- app/Http/Livewire/EditUser.php
+++ app/Http/Livewire/EditUser.php
@@ -34,6 +34,7 @@ class EditUser extends Component
             'user.firstname' => 'required|max:255',
             'user.lastname' => 'required|max:255',
             'user.birthday' => 'required|date',
+            'user.birthplace' => 'nullable|string',
             'user.email' => 'required|max:255|email:rfc'.((App::environment('production'))?',dns,spoof':'').'|unique:App\Models\User,email,'.$this->user->id,
             'user.employer' => 'nullable|string',
             'user.hom_adr' => 'nullable|string',
