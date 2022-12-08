b4fdc52a6b2e99ee0a0107b210b0cfac4a65d758
Guillaume Orsal (acotango) <guillaume@orsal.net>
2022-12-08 14:17:34 +0100

    Time input field and clock icon

---
+++ resources/views/components/input/time.blade.php
@@ -0,0 +1,11 @@
+
+
+<div class="flex rounded-md shadow-sm">
+    <span class="inline-flex items-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 sm:text-sm">
+        <span class="text-gray-400 sm:text-sm sm:leading-5">
+            <x-icon.clock />
+        </span>
+    </span>
+
+    <input {{ $attributes }} type="time" class="form-input border-gray-300 block w-40 text-center text-gray-700 transition duration-150 ease-in-out sm:text-sm sm:leading-5 rounded-r-md" aria-describedby="time">
+</div>
