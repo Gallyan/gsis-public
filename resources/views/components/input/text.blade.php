17a0d0254b8b67ea3a428c7a4a4c8aca0a7a5051
Guillaume Orsal (acotango) <guillaume@orsal.net>
2022-12-08 18:08:00 +0100

    Gestion de la size pour le input text

--- resources/views/components/input/text.blade.php
+++ resources/views/components/input/text.blade.php
@@ -2,12 +2,12 @@
     'leadingAddOn' => false,
 ])
 
-<div class="flex rounded-md shadow-sm">
+<div class="flex rounded-md shadow-sm {{ $attributes['size'] }}">
     @if ($leadingAddOn)
         <span class="inline-flex items-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-400 sm:text-sm">
             {{ $leadingAddOn }}
         </span>
     @endif
 
-    <input {{ $attributes->merge(['class' => 'flex-1 form-input border-cool-gray-300 text-gray-700 block w-full transition duration-150 ease-in-out sm:text-sm sm:leading-5' . ($leadingAddOn ? ' rounded-none rounded-r-md' : '')]) }}/>
+    <input {{ $attributes->except(['size'])->merge(['class' => 'flex-1 form-input border-cool-gray-300 text-gray-700 block w-full transition duration-150 ease-in-out sm:text-sm sm:leading-5' . ($leadingAddOn ? ' rounded-none rounded-r-md' : '')]) }}/>
 </div>
