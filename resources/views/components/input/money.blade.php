c99979f3b50d1d2d75f7ee008c8cfcbc1c00c778
Guillaume Orsal (acotango) <guillaume@orsal.net>
2022-12-21 13:11:18 +0100

    Money input always with icon

--- resources/views/components/input/money.blade.php
+++ resources/views/components/input/money.blade.php
@@ -1,15 +1,7 @@
-@props([
-    'leadingIcon' => true,
-])
-
 <div class="flex rounded-md shadow-sm">
-    @if ($leadingIcon)
-    <span class="inline-flex items-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 sm:text-sm">
-        <span class="text-gray-400 sm:text-sm sm:leading-5">
-            <x-icon.coins />
-        </span>
+    <span class="inline-flex items-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-400 sm:text-sm sm:leading-5">
+        <x-icon.coins />
     </span>
-    @endif
 
-    <input {{ $attributes }} type="number" step="0.01" class="form-input border-gray-300 block w-full text-gray-700 transition duration-150 ease-in-out sm:text-sm sm:leading-5 rounded-r-md @if(!$leadingIcon) rounded-l-md @endif" placeholder="0,00" aria-describedby="price-currency">
+    <input {{ $attributes }} type="number" step="0.01" class="form-input border-gray-300 block w-full text-gray-700 transition duration-150 ease-in-out sm:text-sm sm:leading-5 rounded-r-md" placeholder="0,00" aria-describedby="price-currency">
 </div>
