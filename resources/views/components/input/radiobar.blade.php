83a226a90911c09e73feed19748f95cab8fb15df
Guillaume Orsal (acotango) <guillaume@orsal.net>
2023-01-02 11:25:13 +0100

    Gestion du disabled pour le radiobar

--- resources/views/components/input/radiobar.blade.php
+++ resources/views/components/input/radiobar.blade.php
@@ -1,6 +1,7 @@
 @props([
     'keylabel' => [],
     'selected' => false,
+    'disabled' => false,
 ])
 
 <div {{ $attributes->only(['class'])->merge(['class' => 'border border-gray-300 rounded-md divide-y divide-x divide-gray-300 lg:flex lg:divide-y-0 w-full']) }} >
@@ -12,10 +13,13 @@
         id="{{ $attributes['id'] }}-{{ $key }}"
         type="radio"
         name="{{ $attributes['id'] }}"
-        value="{{ $key }}" />
+        value="{{ $key }}"
+        @if ( $disabled ) disabled @endif
+        />
 
       <label
         class="group flex items-center justify-center w-full cursor-pointer transition duration-300 ease-in-out
+          @if ( $disabled ) bg-gray-300 opacity-75 cursor-not-allowed @endif
           @if ( $selected == $key ) text-indigo-600  hover:text-indigo-800 border-indigo-600
           @else text-gray-500 hover:text-gray-700  border-gray-300 @endif bg-gray-50 hover:bg-gray-100"
         for="{{ $attributes['id'] }}-{{ $key }}" >
