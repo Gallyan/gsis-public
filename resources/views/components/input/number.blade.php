8cfb3dc0243c4605ed718f4cbc1d08a286d9d704
Guillaume Orsal (acotango) <guillaume@orsal.net>
2023-01-02 22:36:54 +0100

    Add leadingIcon to number input

--- resources/views/components/input/number.blade.php
+++ resources/views/components/input/number.blade.php
@@ -1,3 +1,14 @@
+@props([
+    'leadingIcon' => false,
+])
+
 <div class="flex shadow-sm">
-    <input {{ $attributes->merge(['class' => 'flex-1 form-input border-gray-300 text-gray-700 block w-full transition duration-150 ease-in-out sm:text-sm sm:leading-5 rounded-md']) }} type="number" step="1">
+    @if ($leadingIcon)
+        @php $leadingIcon = "icon.".$leadingIcon @endphp
+        <span class="inline-flex items-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-400 sm:text-sm">
+            <x-dynamic-component :component="$leadingIcon" />
+        </span>
+    @endif
+
+    <input {{ $attributes->merge(['class' => 'flex-1 form-input border-gray-300 text-gray-700 block w-full transition duration-150 ease-in-out sm:text-sm sm:leading-5 ' . ($leadingIcon ? 'rounded-r-md' : 'rounded-md')]) }} type="number" step="1">
 </div>
