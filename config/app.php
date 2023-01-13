f29cb2c67a71339a8dd3f0def225969816c7130a
Guillaume Orsal (acotango) <guillaume@orsal.net>
2023-01-13 13:42:43 +0100

    Update version to v1.0.2, role par defaut

--- config/app.php
+++ config/app.php
@@ -51,7 +51,7 @@ return [
     | or any other location as required by the application or its packages.
     */
 
-    'version' => env('APP_VERSION', '1.0.1'),
+    'version' => env('APP_VERSION', '1.0.2'),
 
     /*
     |--------------------------------------------------------------------------
