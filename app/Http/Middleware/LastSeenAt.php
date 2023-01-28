b14534d39f2c8144836bcd388a9f0a3a2dd54407
Guillaume Orsal (acotango) <guillaume@orsal.net>
2023-01-28 23:30:50 +0100

    don't touch updated_at when updating last_seen_at

--- app/Http/Middleware/LastSeenAt.php
+++ app/Http/Middleware/LastSeenAt.php
@@ -4,7 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
-use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\DB;
 
 class LastSeenAt
 {
@@ -18,8 +18,9 @@ class LastSeenAt
     public function handle(Request $request, Closure $next)
     {
         if( auth()->id() ) {
+            auth()->user()->timestamps = false; // Don't update updated_at
             auth()->user()->update([
-                'last_seen_at' => Carbon::now()->toDateTimeString(),
+                'last_seen_at' => DB::raw('now()'),
             ]);
         }
 
