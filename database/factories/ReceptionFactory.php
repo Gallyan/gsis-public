3bf331f5421f3f7b40c1383cc83544ea8e839a64
Guillaume Orsal (acotango) <guillaume@orsal.net>
2023-12-29 18:16:12 +0100

    Less null on reception seeder

--- database/factories/ReceptionFactory.php
+++ database/factories/ReceptionFactory.php
@@ -18,11 +18,11 @@ class ReceptionFactory extends Factory
     {
         return [
             'purchase_id' => fake()->randomElement(\App\Models\Purchase::pluck('id')),
-            'subject' => fake()->optional()->sentence(3),
-            'number' => fake()->optional()->numberBetween(1, 15),
-            'supplier' => fake()->optional()->company,
-            'date' => fake()->optional()->dateTimeBetween('-90 days','90 days')?->format('Y-m-d'),
-            'amount' => fake()->optional()->randomFloat(2,1,1000),
+            'subject' => fake()->optional(weight: 0.8)->sentence(3),
+            'number' => fake()->optional(weight: 0.8)->numberBetween(1, 15),
+            'supplier' => fake()->optional(weight: 0.8)->company,
+            'date' => fake()->optional(weight: 0.8)->dateTimeBetween('-90 days','90 days')?->format('Y-m-d'),
+            'amount' => fake()->optional(weight: 0.8)->randomFloat(2,1,1000),
             'currency' => fake()->randomElement(['EUR', 'USD', 'GBP', 'CHF','BTC']),
             'guests' => [],
         ];
