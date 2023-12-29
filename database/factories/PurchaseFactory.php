ee28c298f10d281d0b34079adeb9b3d8da803793
Guillaume Orsal (acotango) <guillaume@orsal.net>
2023-12-29 17:31:56 +0100

    Purchase Factory

--- database/factories/PurchaseFactory.php
+++ database/factories/PurchaseFactory.php
@@ -16,8 +16,26 @@ class PurchaseFactory extends Factory
      */
     public function definition(): array
     {
+        $institution = \App\Models\Institution::inRandomOrder()->first();
+
         return [
-            //
+            'user_id' => fake()->randomElement(\App\Models\User::pluck('id')),
+            'subject' => fake()->sentence(),
+            'institution_id' => $institution->id,
+            'wp' => $institution->wp ? fake()->numberBetween(1, 15) : null,
+            'miscs' => fake()->optional(default: [])->passthrough(array_map(function () {
+                            return [
+                                'subject' => fake()->sentence(3),
+                                'supplier' => fake()->company,
+                                'date' => fake()->dateTimeBetween('-90 days','90 days')->format('Y-m-d'),
+                                'miscamount' => fake()->randomFloat(2,1,1000),
+                                'currency' => fake()->randomElement(['EUR', 'USD', 'GBP', 'CHF','BTC']),
+                            ];
+                        }, range(1, fake()->numberBetween(1, 5)))),
+            'comments' => fake()->optional()->text(500),
+            'status' => fake()->randomElement(array_keys(\App\Models\Purchase::STATUSES)),
+            'amount' => fake()->optional()->randomFloat(2,1,10000),
+            'created_at' => fake()->dateTimeBetween('-90 days'),
         ];
     }
 }
